856db65da17d950b1b80046e5df24ff8657de43f
unknown <lepiedrahita5@misena.edu.com>
2021-03-29 10:32:29 -0500

    agregamos el index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+
+
+
+<!-- jQuery, Popper.js, Bootstrap JS -->
+
+
+         
+
+<!--FIN del cont principal-->
+
+<?php require_once "vistas/parte_inferior.php"?>
